64cf4df1545ebb718f5b1ce6180f7a2588cc84e8
tonytomo <fathonisatrioutomo@gmail.com>
2025-03-19 14:37:21 +0700

    feat: add GitHub Actions workflow for deployment to GitHub Pages; update project version and configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -8,7 +10,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
